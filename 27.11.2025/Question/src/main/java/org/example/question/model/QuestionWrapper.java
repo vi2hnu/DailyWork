6cab9648e518ec40676d303cb38122424fc5d13e
vi2hnu <vishnuvaradhankr@gmail.com>
2025-11-28 15:03:33 +0530

    fixed type mismatch issue

--- 27.11.2025/Question/src/main/java/org/example/question/model/QuestionWrapper.java
+++ 27.11.2025/Question/src/main/java/org/example/question/model/QuestionWrapper.java
@@ -14,7 +14,7 @@ public class QuestionWrapper {
     private String option3;
     private String option4;
 
-    public QuestionWrapper(Integer id, String questionTitle, String option1, String option2, String option3, String option4) {
+    public QuestionWrapper(String id, String questionTitle, String option1, String option2, String option3, String option4) {
         this.id = id;
         this.questionTitle = questionTitle;
         this.option1 = option1;
