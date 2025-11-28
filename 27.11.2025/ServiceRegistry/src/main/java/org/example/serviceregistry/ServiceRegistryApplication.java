257f97200a9c70d9259e0378a248888db7c4c448
vi2hnu <vishnuvaradhankr@gmail.com>
2025-11-28 15:03:47 +0530

    added service registry

---
+++ 27.11.2025/ServiceRegistry/src/main/java/org/example/serviceregistry/ServiceRegistryApplication.java
@@ -0,0 +1,15 @@
+package org.example.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
+}
