a040c2e217c5a18e58fc0c88ec43346411981567
vi2hnu <vishnuvaradhankr@gmail.com>
2025-12-11 16:10:35 +0530

    work don on 11.12.2025

---
+++ 11.12.2025/FlieExercise/src/main/java/org/example/flieexercise/FlieExerciseApplication.java
@@ -0,0 +1,13 @@
+package org.example.flieexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlieExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlieExerciseApplication.class, args);
+	}
+
+}
---
+++ 11.12.2025/FlieExercise/src/test/java/org/example/flieexercise/FlieExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.flieexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlieExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
